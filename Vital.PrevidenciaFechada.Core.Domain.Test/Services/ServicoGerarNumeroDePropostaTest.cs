98486c513caaf4bd191589a2604bb43b21448a34
Julio Cesar C. Cunha <jcesar_cunha@yahoo.com.br>
2013-04-01 11:43:32 -0300

    #MAAB-127 - Serviço de geração de número de proposta

---
+++ Vital.PrevidenciaFechada.Core.Domain.Test/Services/ServicoGerarNumeroDePropostaTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using System.Collections.Generic;
+using Vital.PrevidenciaFechada.Core.Domain.Entities.ComponenteProposta;
+using Vital.PrevidenciaFechada.Core.Domain.Repository;
+using Vital.PrevidenciaFechada.Core.Domain.Services;
+
+namespace Vital.PrevidenciaFechada.Core.Domain.Test.Services
+{
+	[TestFixture]
+	public class ServicoGerarNumeroDePropostaTest
+	{
+		private IRepositorio<Proposta> _repositorio;
+		private ServicoGerarNumeroDeProposta _servico;
+
+		[SetUp]
+		public void iniciar()
+		{
+			_repositorio = MockRepository.GenerateMock<IRepositorio<Proposta>>();
+			_servico = new ServicoGerarNumeroDeProposta(_repositorio);
+		}
+
+		[Test]
+		public void obter_ultimo_numero_gerado_retorna_1_se_nao_houver_nenhuma_proposta()
+		{
+			_repositorio.Expect(x => x.Todas()).Return(new List<Proposta>());
+
+			string numeroGerado = _servico.GerarNumeroDeProposta();
+
+			Assert.That(numeroGerado, Is.EqualTo("1"));
+		}
+
+		[Test]
+		public void obter_ultimo_numero_gerado_retorna_o_maior_numero_de_proposta_cadastrada()
+		{
+			List<Proposta> listaRetorno = new List<Proposta>
+			{
+				new Proposta { Numero = "10" },
+				new Proposta { Numero = "2" },
+				new Proposta { Numero = "3" }
+			};
+
+			_repositorio.Expect(x => x.Todas()).Return(listaRetorno);
+
+			string numeroGerado = _servico.GerarNumeroDeProposta();
+
+			Assert.That(numeroGerado, Is.EqualTo("11"));
+		}
+	}
+}
---
+++ Vital.PrevidenciaFechada.Core.Domain/Services/ServicoGerarNumeroDeProposta.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vital.InfraStructure.DSL.DesignByContract;
+using Vital.PrevidenciaFechada.Core.Domain.Entities.ComponenteProposta;
+using Vital.PrevidenciaFechada.Core.Domain.Repository;
+
+namespace Vital.PrevidenciaFechada.Core.Domain.Services
+{
+	/// <summary>
+	/// Serviço de domínio para geração de número de proposta sequencial
+	/// </summary>
+	public class ServicoGerarNumeroDeProposta
+	{
+		/// <summary>
+		/// Repositório de Proposta
+		/// </summary>
+		private IRepositorio<Proposta> _repositorio;
+
+		/// <summary>
+		/// Construtor do serviço de domínio injetando o repositório de proposta
+		/// </summary>
+		public ServicoGerarNumeroDeProposta(IRepositorio<Proposta> repositorio)
+		{
+			#region Pré-condições
+
+			IAssertion oRepositorioFoiInformado = Assertion.NotNull(repositorio, "O repositório não foi injetado corretamente");
+
+			#endregion
+
+			oRepositorioFoiInformado.Validate();
+
+			_repositorio = repositorio;
+
+			#region Pós-condições
+
+			IAssertion oRepositorioFoiInjetadoCoretamente = Assertion.Equals(_repositorio, repositorio, "O repositório da classe não está igual ao repositório injetado");
+
+			#endregion
+
+			oRepositorioFoiInjetadoCoretamente.Validate();
+		}
+
+		/// <summary>
+		/// Gera um novo número sequencial para a proposta
+		/// </summary>
+		/// <returns></returns>
+		public virtual string GerarNumeroDeProposta()
+		{
+			return (ObterUltimoNumeroGerado() + 1).ToString();
+		}
+
+		/// <summary>
+		/// Obtém o último número sequencial de proposta gerado
+		/// </summary>
+		/// <returns></returns>
+		private int ObterUltimoNumeroGerado()
+		{
+			#region Pré-condições
+
+			IAssertion oRepositorioFoiInjetadoNoServico = Assertion.NotNull(_repositorio, "O repositório não foi injetado corretamente");
+
+			#endregion
+
+			oRepositorioFoiInjetadoNoServico.Validate();
+
+			int numeroGerado = default(int);
+			var propostas = _repositorio.Todas();
+
+			if (propostas.Count > 0)
+				numeroGerado = ObterMaiorNumeroDeProposta(propostas);
+
+			return numeroGerado;
+		}
+
+		/// <summary>
+		/// Obtém o maior número entre as propostas existentes
+		/// </summary>
+		/// <param name="propostas"></param>
+		/// <returns></returns>
+		private int ObterMaiorNumeroDeProposta(IList<Proposta> propostas)
+		{
+			return propostas.Max(p => Convert.ToInt32(p.Numero));
+		}
+	}
+}
