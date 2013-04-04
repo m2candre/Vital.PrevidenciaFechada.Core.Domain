7c54b6cf42b05bf5ec85d506673fdde662e35ab2
Julio Cesar C. Cunha <jcesar_cunha@yahoo.com.br>
2013-04-04 18:10:48 -0300

    #MAAB-128 - Método ObterPor no repositório de convênio de adesão

--- Vital.PrevidenciaFechada.Core.Domain/Entities/ComponenteConvenioDeAdesao/IRepositorioConvenioDeAdesao.cs
+++ Vital.PrevidenciaFechada.Core.Domain/Entities/ComponenteConvenioDeAdesao/IRepositorioConvenioDeAdesao.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vital.PrevidenciaFechada.Core.Domain.Entities.ComponentePlano;
+using Vital.PrevidenciaFechada.Core.Domain.Repository;
 
 namespace Vital.PrevidenciaFechada.Core.Domain.Entities.ComponenteConvenioDeAdesao
 {
     /// <summary>
     /// Repositório de Convênio de Adesão
     /// </summary>
-    public interface IRepositorioConvenioDeAdesao
+    public interface IRepositorioConvenioDeAdesao : IRepositorio<ConvenioDeAdesao>
     {
         /// <summary>
         /// Último número de proposta
@@ -18,5 +20,13 @@ namespace Vital.PrevidenciaFechada.Core.Domain.Entities.ComponenteConvenioDeAdes
         /// <param name="PessoaJuridicaId">PessoaJuridicaId</param>
         /// <returns>int</returns>
         int UltimoNumeroDeProposta(Guid PlanoId, Guid PessoaJuridicaId);
+
+		/// <summary>
+		/// Obter convênio por plano e pessoa jurídica do contexto do usuário
+		/// </summary>
+		/// <param name="PlanoId"></param>
+		/// <param name="PessoaJuridicaId"></param>
+		/// <returns></returns>
+		ConvenioDeAdesao ObterPor(Guid PlanoId, Guid PessoaJuridicaId);
     }
 }
--- Vital.PrevidenciaFechada.Core.Domain/Repository/RepositorioConvenioDeAdesao.cs
+++ Vital.PrevidenciaFechada.Core.Domain/Repository/RepositorioConvenioDeAdesao.cs
@@ -10,37 +10,63 @@ using Vital.PrevidenciaFechada.Core.Domain.Entities.ComponenteProposta;
 
 namespace Vital.PrevidenciaFechada.Core.Domain.Repository
 {
-    /// <summary>
-    /// Repositório de Convênio de Adesão
-    /// </summary>
-    public class RepositorioConvenioDeAdesao : Repositorio<ConvenioDeAdesao>, IRepositorioConvenioDeAdesao
-    {
-        /// <summary>
-        /// Último número de proposta
-        /// </summary>
-        /// <param name="PlanoId">PlanoId</param>
-        /// <param name="PessoaJuridicaId">PessoaJuridicaId</param>
-        /// <returns>int</returns>
-        public int UltimoNumeroDeProposta(Guid PlanoId, Guid PessoaJuridicaId)
-        {
-            //PLANO, PESSOAJURIDICA, Proposta, Convenio de Adesão
+	/// <summary>
+	/// Repositório de Convênio de Adesão
+	/// </summary>
+	public class RepositorioConvenioDeAdesao : Repositorio<ConvenioDeAdesao>, IRepositorioConvenioDeAdesao
+	{
+		/// <summary>
+		/// Último número de proposta
+		/// </summary>
+		/// <param name="PlanoId">PlanoId</param>
+		/// <param name="PessoaJuridicaId">PessoaJuridicaId</param>
+		/// <returns>int</returns>
+		public int UltimoNumeroDeProposta(Guid PlanoId, Guid PessoaJuridicaId)
+		{
+			//PLANO, PESSOAJURIDICA, Proposta, Convenio de Adesão
 
-            var criterio = Session.CreateCriteria(typeof(ConvenioDeAdesao), "c")
-                  .CreateCriteria("c.PessoaJuridica", "pj", NHibernate.SqlCommand.JoinType.InnerJoin)
-                  .Add(Expression.Eq("pj.Id", PessoaJuridicaId));
+			var criterio = Session.CreateCriteria(typeof(ConvenioDeAdesao), "c")
+				  .CreateCriteria("c.PessoaJuridica", "pj", NHibernate.SqlCommand.JoinType.InnerJoin)
+				  .Add(Expression.Eq("pj.Id", PessoaJuridicaId));
 
-            var convenios = criterio.List<ConvenioDeAdesao>();
+			var convenios = criterio.List<ConvenioDeAdesao>();
 
-            var criterioPlano = Session.CreateCriteria(typeof(Plano), "p")
-                .Add(Expression.Eq("p.Id", PlanoId));
+			var criterioPlano = Session.CreateCriteria(typeof(Plano), "p")
+				.Add(Expression.Eq("p.Id", PlanoId));
 
-            var plano = criterioPlano.UniqueResult<Plano>();
+			var plano = criterioPlano.UniqueResult<Plano>();
 
-            int ultimoNumeroDaProposta = (from cp in plano.ConvenioDeAdesao
-                                             join c in convenios on cp.Id equals c.Id
-                                             select c.Propostas.Max(x=> x.Numero)).SingleOrDefault();
+			int ultimoNumeroDaProposta = (from cp in plano.ConvenioDeAdesao
+										  join c in convenios on cp.Id equals c.Id
+										  select c.Propostas.Max(x => x.Numero)).SingleOrDefault();
 
-            return ultimoNumeroDaProposta;
-        }
-    }
+			return ultimoNumeroDaProposta;
+		}
+
+		/// <summary>
+		/// Obter convênio por plano e pessoa jurídica do contexto do usuário
+		/// </summary>
+		/// <param name="PlanoId"></param>
+		/// <param name="PessoaJuridicaId"></param>
+		/// <returns></returns>
+		public ConvenioDeAdesao ObterPor(Guid PlanoId, Guid PessoaJuridicaId)
+		{
+			var criterio = Session.CreateCriteria(typeof(ConvenioDeAdesao), "c")
+				  .CreateCriteria("c.PessoaJuridica", "pj", NHibernate.SqlCommand.JoinType.InnerJoin)
+				  .Add(Expression.Eq("pj.Id", PessoaJuridicaId));
+
+			var convenios = criterio.List<ConvenioDeAdesao>();
+
+			var criterioPlano = Session.CreateCriteria(typeof(Plano), "p")
+				.Add(Expression.Eq("p.Id", PlanoId));
+
+			var plano = criterioPlano.UniqueResult<Plano>();
+
+			ConvenioDeAdesao convenio = (from cp in plano.ConvenioDeAdesao
+										 join c in convenios on cp.Id equals c.Id
+										 select c).SingleOrDefault();
+
+			return convenio;
+		}
+	}
 }
