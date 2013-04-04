206d3a4b50ddbe9655c15d28957d49d893690f5b
Julio Cesar C. Cunha <jcesar_cunha@yahoo.com.br>
2013-04-04 17:25:20 -0300

    #MAAB-128 - Método ObterCriticasDaProposta no ServicoProposta

--- Vital.PrevidenciaFechada.Core.Domain/Services/ServicoProposta.cs
+++ Vital.PrevidenciaFechada.Core.Domain/Services/ServicoProposta.cs
@@ -9,6 +9,7 @@ using Vital.PrevidenciaFechada.Core.Domain.Entities.ComponenteProposta;
 using Vital.PrevidenciaFechada.Core.Domain.Repository;
 using Vital.PrevidenciaFechada.DTO.Messages.Core;
 using Vital.Extensions.DateTimeExtensions;
+using Vital.PrevidenciaFechada.Core.Domain.Mappers;
 
 namespace Vital.PrevidenciaFechada.Core.Domain.Services
 {
@@ -111,6 +112,22 @@ namespace Vital.PrevidenciaFechada.Core.Domain.Services
 			#endregion
 
 			return propostasEncontradas.ToList();
+		}
+
+		/// <summary>
+		/// Obtem uma lista de críticas da proposta
+		/// </summary>
+		/// <param name="propostaDTO">proposta</param>
+		/// <returns>Lista de críticas</returns>
+		public virtual IList<CriticaDTO> ObterCriticasDaProposta(PropostaDTO propostaDTO, Guid IdDoPlano)
+		{
+			var plano = _repositorioPlano.PorId(IdDoPlano);
+
+			var propostaVO = new PropostaMapper().DePropostaDTOParaPropostaVO(propostaDTO);
+
+			IList<CriticaVO> criticasVO = plano.Regulamento.ObterCriticasDaProposta(propostaVO);
+
+			return new CriticaMapper().DeCriticaVOParaCriticaDTO(criticasVO);
 		}
 
 		/// <summary>
