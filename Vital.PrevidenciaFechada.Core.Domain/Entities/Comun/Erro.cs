e9cd7c6e471ec0df5c8992e231188ec3bee56aa7
Isaac Garcia <isaac@workker.com.br>
2013-04-12 15:55:30 -0300

    Servico obter ultimo codigo de erro

---
+++ Vital.PrevidenciaFechada.Core.Domain/Entities/Comun/Erro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vital.PrevidenciaFechada.Core.Domain.Entities.Comun
+{
+    /// <summary>
+    /// Erro
+    /// </summary>
+    public class Erro : IAggregateRoot<Guid>
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public virtual Guid Id { get; set; }
+
+        /// <summary>
+        /// Número
+        /// </summary>
+        public virtual int Numero { get; set; }
+    }
+}
---
+++ Vital.PrevidenciaFechada.Core.Domain/Services/ServicoObterUltimoCodigoDeErro.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vital.PrevidenciaFechada.Core.Domain.Entities.Comun;
+using Vital.PrevidenciaFechada.Core.Domain.Repository;
+
+namespace Vital.PrevidenciaFechada.Core.Domain.Services
+{
+    /// <summary>
+    /// Serviço para obter último código de erro
+    /// </summary>
+    public class ServicoObterUltimoCodigoDeErro
+    {
+        private IRepositorio<Erro> _erros;
+
+        /// <summary>
+        /// Construtor
+        /// </summary>
+        /// <param name="erros">repositório de erros</param>
+        public ServicoObterUltimoCodigoDeErro(IRepositorio<Erro> erros)
+        {
+            _erros = erros;
+        }
+
+        /// <summary>
+        /// Obtem o último numero de erro
+        /// </summary>
+        /// <returns></returns>
+        public int Obter()
+        {
+            var erro = _erros.Todos().FirstOrDefault();
+
+            if (erro == null)
+            {
+                erro = new Erro() { Numero = 1 };
+                _erros.Adicionar(erro);
+            }
+            else
+            {
+                erro.Numero = erro.Numero + 1;
+                _erros.Adicionar(erro);
+            }
+
+            return erro.Numero;
+        }
+    }
+}
