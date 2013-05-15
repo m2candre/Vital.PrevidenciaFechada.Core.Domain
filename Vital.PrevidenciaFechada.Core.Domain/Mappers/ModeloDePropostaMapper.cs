91380ef9e5f2801e30b7017d72b3e74f6c58707c
Rodrigo Caldas <rcaldasmd@gmail.com>
2013-05-15 16:38:49 -0300

    Incluso classe ModeloDePropostaMapper

---
+++ Vital.PrevidenciaFechada.Core.Domain/Mappers/ModeloDePropostaMapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Vital.PrevidenciaFechada.DTO.Messages.Core;
+using Vital.PrevidenciaFechada.Core.Domain.Entities.ComponentePlano;
+using Vital.PrevidenciaFechada.DTO.Messages.Core;
+
+namespace Vital.PrevidenciaFechada.Core.Domain.Mappers
+{
+    public class ModeloDePropostaMapper
+    {
+        public ModeloDePropostaDTO ObterModeloDaPropostaDTO(ModeloDeProposta modeloDaProposta)
+        {
+            return Mapper.Map<ModeloDeProposta, ModeloDePropostaDTO>(modeloDaProposta);
+        }
+
+        public ModeloDeProposta ObterModeloDaProposta(ModeloDePropostaDTO modeloDaPropostaDTO)
+        {
+            return Mapper.Map<ModeloDePropostaDTO, ModeloDeProposta>(modeloDaPropostaDTO);
+        }
+    }
+}
