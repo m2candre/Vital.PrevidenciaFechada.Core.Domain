e6451195f962864e44455e401f1364bdd740921e
Rodrigo Caldas <rcaldasmd@gmail.com>
2013-05-16 19:16:18 -0300

    Inclusão do Serviço de Participante

---
+++ Vital.PrevidenciaFechada.Core.Domain/Services/ServicoParticipante.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vital.PrevidenciaFechada.Core.Domain.Entities.ComponenteParticipante;
+
+namespace Vital.PrevidenciaFechada.Core.Domain.Services
+{
+    /// <summary>
+    /// Serviço de domínio para entidade Participante
+    /// </summary>
+    public class ServicoParticipante
+    {
+        public virtual List<string> ObterPropriedadesDoParticipante()
+        {
+            List<string> lista = new List<string>();
+            foreach (var propriedade in typeof(Participante).GetProperties())
+            {
+                lista.Add(propriedade.Name);
+            }
+            return lista;
+        }
+    }
+}
