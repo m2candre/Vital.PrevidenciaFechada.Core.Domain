44605bd9e2742052d2f367918882cd69e648b5af
Rodrigo Caldas <rcaldasmd@gmail.com>
2013-05-16 19:15:23 -0300

    Adição do construtor do ModeloDePropostaMapper

--- Vital.PrevidenciaFechada.Core.Domain/Mappers/ModeloDePropostaMapper.cs
+++ Vital.PrevidenciaFechada.Core.Domain/Mappers/ModeloDePropostaMapper.cs
@@ -6,12 +6,17 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Vital.PrevidenciaFechada.DTO.Messages.Core;
 using Vital.PrevidenciaFechada.Core.Domain.Entities.ComponentePlano;
-using Vital.PrevidenciaFechada.DTO.Messages.Core;
 
 namespace Vital.PrevidenciaFechada.Core.Domain.Mappers
 {
     public class ModeloDePropostaMapper
     {
+        public ModeloDePropostaMapper()
+        {
+            Mapper.CreateMap<ModeloDeProposta, ModeloDePropostaDTO>();
+            Mapper.CreateMap<ModeloDePropostaDTO, ModeloDeProposta>();
+        }
+
         public ModeloDePropostaDTO ObterModeloDaPropostaDTO(ModeloDeProposta modeloDaProposta)
         {
             return Mapper.Map<ModeloDeProposta, ModeloDePropostaDTO>(modeloDaProposta);
