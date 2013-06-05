d3b41daf0f81b115802ea7c56a6cf614d0c87d9e
Rodrigo Caldas <rcaldasmd@gmail.com>
2013-06-05 15:25:21 -0300

    Adicionado "UsuarioMapper"

---
+++ Vital.PrevidenciaFechada.Core.Domain/Mappers/UsuarioMapper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vital.PrevidenciaFechada.Core.Domain.Entities.ComponenteEntidade;
+using Vital.PrevidenciaFechada.Core.Domain.Entities.ComponenteUsuario;
+using Vital.PrevidenciaFechada.DTO.Messages;
+using Vital.PrevidenciaFechada.Core.Domain.Entities.ComponentePessoaJuridica;
+using Vital.PrevidenciaFechada.DTO.Messages.Core;
+
+namespace Vital.PrevidenciaFechada.Core.Domain.Mappers
+{
+    /// <summary>
+    /// Mapea Usuários com DTOs específico
+    /// </summary>
+    public class UsuarioMapper
+    {
+        /// <summary>
+        /// Construtor definindo o esquema de mapping
+        /// </summary>
+        public UsuarioMapper()
+        {
+			Mapper.CreateMap<Usuario, UsuarioDTO>();
+            Mapper.CreateMap<UsuarioDTO, Usuario>();
+        }
+
+        /// <summary>
+        /// Mapeia um Usuário em UsuarioDTO
+        /// </summary>
+        /// <param name="identidade">Objeto de Identidade para mapeamento</param>
+        /// <returns></returns>
+        public virtual UsuarioDTO ObterDTO(Usuario usuario)
+        {
+            return Mapper.Map<Usuario, UsuarioDTO>(usuario);
+        }
+
+        /// <summary>
+        /// Mapeia um UsuárioDTO em Usuario
+        /// </summary>
+        /// <param name="dto">Objeto de Entidade para mapeamento</param>
+        /// <returns></returns>
+        public virtual Usuario ObterEntidade(UsuarioDTO usuarioDTO)
+        {
+            return Mapper.Map<UsuarioDTO, Usuario>(usuarioDTO);
+        }
+
+        /// <summary>
+        /// Mapeia uma lista de Usuários em uma Lista de UsuarioDTO
+        /// </summary>
+        /// <param name="usuarios">Lista das Identidades dos usuários cadastrados</param>
+        /// <returns></returns>
+        public virtual List<UsuarioDTO> ObterListaDeUsuarioDTO(List<Usuario> usuarios)
+        {
+            var usuariosDTO = new List<UsuarioDTO>();
+            usuarios.ForEach(usr => usuariosDTO.Add(ObterDTO(usr)));
+            return usuariosDTO;
+        }
+    }
+}
