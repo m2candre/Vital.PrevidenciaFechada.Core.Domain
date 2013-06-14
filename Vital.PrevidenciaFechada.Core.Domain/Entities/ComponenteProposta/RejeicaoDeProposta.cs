1b015e58cfc77ac5e7fa544496b7db907f19e902
Vinícius Aragão <viniciusaragao@gmail.com>
2013-06-14 14:19:31 -0300

    Rejeição de proposta

---
+++ Vital.PrevidenciaFechada.Core.Domain/Entities/ComponenteProposta/RejeicaoDeProposta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vital.PrevidenciaFechada.Core.Domain.Entities.ComponenteProposta
+{
+    public class RejeicaoDeProposta : IAggregateRoot<Guid>
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public virtual Guid Id { get; set; }
+
+        /// <summary>
+        /// ID do Convênio de Adesão
+        /// </summary>
+        public virtual Guid IdDoConvenioDeAdesao { get; set; }
+
+        /// <summary>
+        /// Tipo de rejeicao
+        /// </summary>
+        public virtual TipoRejeicao TipoRejeicao { get; set; }
+
+        /// <summary>
+        /// Descrição
+        /// </summary>
+        public virtual string Motivo { get; set; }
+
+
+    }
+}
---
+++ Vital.PrevidenciaFechada.Core.Domain/Mappers/RejeicaoDePropostaMapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Vital.PrevidenciaFechada.Core.Domain.Entities.ComponenteProposta;
+using Vital.PrevidenciaFechada.Core.Domain.Entities.ComponenteUsuario;
+using Vital.PrevidenciaFechada.DTO.Messages;
+using Vital.PrevidenciaFechada.DTO.Messages.Atuarial.InscricaoEmPapel;
+using Vital.PrevidenciaFechada.DTO.Messages.Comun;
+
+namespace Vital.PrevidenciaFechada.Core.Domain.Mappers
+{
+    public class RejeicaoDePropostaMapper
+    {
+        public RejeicaoDePropostaMapper()
+        {
+            Mapper.CreateMap<RejeicaoDeProposta, RejeicaoDePropostaDTO>();
+            Mapper.CreateMap<RejeicaoDePropostaDTO, RejeicaoDeProposta>();
+            Mapper.CreateMap<TipoRejeicao, TipoRejeicaoDTO>();
+            Mapper.CreateMap<TipoRejeicaoDTO, TipoRejeicao>();
+        }
+
+        public virtual RejeicaoDeProposta ObterEntidade(RejeicaoDePropostaDTO rejeicaoDePropostaDTO)
+        {
+            return Mapper.Map<RejeicaoDePropostaDTO, RejeicaoDeProposta>(rejeicaoDePropostaDTO);
+        }
+    }
+}
