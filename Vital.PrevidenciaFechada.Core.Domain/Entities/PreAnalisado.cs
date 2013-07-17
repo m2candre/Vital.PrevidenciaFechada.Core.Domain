a63163cc6ad6c8c4aff34f6c646901d12ac25b61
Julio Cesar C. Cunha <jcesar_cunha@yahoo.com.br>
2013-07-17 11:20:20 -0300

    Inclusão da entidade PreAnalisado

---
+++ Vital.PrevidenciaFechada.Core.Domain/Entities/PreAnalisado.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Vital.PrevidenciaFechada.Core.Domain.Entities.ComponenteProposta;
+
+namespace Vital.PrevidenciaFechada.Core.Domain.Entities
+{
+	/// <summary>
+	/// Pré-Analisado
+	/// </summary>
+	public class PreAnalisado : IAggregateRoot<Guid>
+	{
+		/// <summary>
+		/// Id
+		/// </summary>
+		public virtual Guid Id { get; set; }
+
+		/// <summary>
+		/// Id do Convênio
+		/// </summary>
+		public virtual Guid IdDoConvenioDeAdesao { get; set; }
+
+		/// <summary>
+		/// CPF
+		/// </summary>
+		public virtual string Cpf { get; set; }
+
+		/// <summary>
+		/// Nome
+		/// </summary>
+		public virtual string Nome { get; set; }
+
+		/// <summary>
+		/// Matrícula
+		/// </summary>
+		public virtual string Matricula { get; set; }
+
+		/// <summary>
+		/// Data de Registro
+		/// </summary>
+		public virtual DateTime? DataDeRegistro { get; set; }
+
+		/// <summary>
+		/// Observação
+		/// </summary>
+		public virtual string Observacao { get; set; }
+
+		/// <summary>
+		/// Tipo de Rejeição
+		/// </summary>
+		public virtual TipoRejeicao TipoDeRejeicao { get; set; }
+
+		/// <summary>
+		/// Construtor
+		/// </summary>
+		public PreAnalisado()
+		{
+
+		}
+	}
+}
